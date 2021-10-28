fc463e8cfcf20a75cadcd722cd7ed06f10acc133
Nathan Houzet <nathan.houzet.etu@univ-lille.fr>
2021-10-28 18:19:53 +0200

    fix : bug avec les ajouts d'éléments dans les liste /!\ -> python dans une classe ne pas mettre les attributs avec le meme nom que les fonctions ...

--- tp.py
+++ tp.py
@@ -1,57 +1,57 @@
-
-
-class cron():
+class Cron:
 
     def __init__(self):
-        self.funk=""
-        self.daysOfWeek  = []
-        self.months      = []
-        self.daysOfMonth = []
-        self.hours       = []
-        self.minutes     = []
+        self._funk = ""
+        self._daysOfWeek = []
+        self._months = []
+        self._daysOfMonth = []
+        self._hours = []
+        self._minutes = []
 
-
-    def funk(s, self):
-        self.funk=s
+    def funk(self, s):
+        self._funk = s
         return self
 
-    def daysOfWeek(m, self):
-        self.daysOfWeek.append(m)
+    def daysOfWeek(self, m):
+        self._daysOfWeek.append(m)
         return self
 
-    def months(m, self):
-        self.months.append(m)
+    def months(self, m):
+        self._months.append(m)
         return self
 
-    def daysOfMonth(m, self):
-        self.daysOfMonth.append(m)
+    def daysOfMonth(self, m):
+        self._daysOfMonth.append(m)
         return self
 
-    def hours(m, self):
-        self.hours.append(m)
+    def hours(self, m):
+        self._hours.append(m)
         return self
 
-    def minutes(m, self):
-        self.minutes.append(m)
+    def minutes(self, m):
+        self._minutes.append(m)
         return self
-    
-    def iterateCron(var):
+
+    def iterateCron(self, var):
         res = ""
         for m in var:
-            if(isinstance(m,int)):
-                res = res + m
-            elif(isinstance(m,tuple)):
+            if isinstance(m, int):
+                res = res + str(m)
+            elif isinstance(m, tuple):
                 res = res + m[0] + '-' + m[1]
-            elif(isinstance(m,list)):
+            elif isinstance(m, list):
                 for mm in m:
                     res = res + mm
-            elif(m == '*'):
+            elif m == '*':
                 res = res + '*'
             res = res + ','
         return res[:-1] + ' '
-    
+
     def generateCron(self):
-        return iterateCron(minutes) + iterateCron(hours) + iterateCron(daysOfMonth) + iterateCron(months) + iterateCron(daysOfWeek) + funk
+        return self.iterateCron(self._minutes) + self.iterateCron(self._hours) + self.iterateCron(self._daysOfMonth) + \
+               self.iterateCron(self._months) + self.iterateCron \
+                   (self._daysOfWeek) + self._funk
 
 
-cron.minutes(15).minutes(16)
+cron=Cron().minutes(12).hours(23)
+print(cron.generateCron())
