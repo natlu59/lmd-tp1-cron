ab630356290890c620905118340f99bdacba61b7
Nathan Houzet <nathan.houzet.etu@univ-lille.fr>
2021-11-18 22:11:32 +0100

    feat : ajout cron nathan

--- nathan/cron.py
+++ nathan/cron.py
@@ -1,4 +1,6 @@
 import calendar
+
+
 class Cron:
 
     def __init__(self):
@@ -10,17 +12,17 @@ class Cron:
 
     def add_minute(self, min_tab):
         for e in min_tab:
-            assert (e == "*" or 0 <= e <= 59 )
+            assert (e == "*" or 0 <= e <= 59)
         self.minutes += min_tab
 
     def add_hour(self, hour_tab):
         for e in hour_tab:
-            assert (e == "*" or 0 <= e <= 23 )
+            assert (e == "*" or 0 <= e <= 23)
         self.hours += hour_tab
 
     def add_day_month(self, day_month):
         for e in day_month:
-            assert (e == "*" or 1 <= e <= 31 )
+            assert (e == "*" or 1 <= e <= 31)
         self.day_month += day_month
 
     def add_month(self, month):
@@ -57,35 +59,35 @@ class Cron:
     def express_day_week(self):
         day_week = "on "
         for e in self.day_week:
-            if e=="*":
-                day_week += "every day"  + " and "
+            if e == "*":
+                day_week += "every day" + " and "
             else:
                 day_week += (calendar.day_name[e]) + " and "
-        return day_week[:-4] if len(self.day_week)>0 else ""
+        return day_week[:-4] if len(self.day_week) > 0 else ""
 
     def express_month(self):
         months = "on "
         for e in self.month:
-            if e=="*":
+            if e == "*":
                 months += "every month "
             else:
                 months += (str(calendar.month_name[e]) + " and ")
         return months[:-4]
 
     def express(self):
-        return "this task is scheduled " + self.express_min()\
-                                         +self.express_hour()\
-                                         +self.express_day_month()\
-                                         +self.express_month()\
-                                         +self.express_day_week()
+        return "this task is scheduled " + self.express_min() \
+               + self.express_hour() \
+               + self.express_day_month() \
+               + self.express_month() \
+               + self.express_day_week()
 
 
 cron = Cron()
-cron.add_minute([5,25])
+cron.add_minute([5, 25])
 
 cron.add_hour([12])
 cron.add_day_month([12])
 cron.add_month([6])
-cron.add_day_week([1,6])
+cron.add_day_week([1, 6])
 
 print(cron.express())
